083e8d55084e8d10781c3cbcec7e1f6f5462bd20
mohsin-8 <alikhanmohsin420@gmail.com>
2024-10-03 03:36:38 +0500

    update base url on live

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://practice-crm-ashen.vercel.app'
+    baseURL: 'https://practice-crm-beige.vercel.app'
 });
 
 export default axiosInstance;
