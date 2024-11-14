85c8ff968442b3c77b2c7d999722aa365ec0bed4
Mohsin Ali Khan <55765160+mohsin-8@users.noreply.github.com>
2024-11-13 23:11:40 -0800

    Update axiosInstance.js

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'https://practice-crm-server.vercel.app',
+    baseURL: 'https://practice-crm-beige.vercel.app',
 });
 
 export default axiosInstance;
