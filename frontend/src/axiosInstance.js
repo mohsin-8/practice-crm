60788c358002778b69283cf9659a3086d0271c68
mohsin-8 <alikhanmohsin420@gmail.com>
2024-11-15 22:02:52 +0500

    change frontend url

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:8080',
-    baseURL: 'https://practice-crm-beige.vercel.app',
+    baseURL: 'https://practice-crm-server.vercel.app',
 });
 
 export default axiosInstance;
