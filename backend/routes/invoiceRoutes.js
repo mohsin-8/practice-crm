7135ffa77625a06aedb8725b5b4d16ef55305475
mohsin-8 <alikhanmohsin420@gmail.com>
2025-02-10 01:29:55 -0800

    create routes for invoice api

--- backend/routes/invoiceRoutes.js
+++ backend/routes/invoiceRoutes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { InvoiceCreate } = require("../controllers/invoiceController");
+const { InvoiceCreate, InvoiceGet, InvoiceGetById, InvoiceUpdate, InvoiceDelete } = require("../controllers/invoiceController");
 
 router.post("/invoice/create", InvoiceCreate);
+router.get("/invoice/get-all", InvoiceGet);
+router.get("/invoice/get-invoice-by-id", InvoiceGetById);
+router.put("/invoice/update-invoice", InvoiceUpdate);
+router.delete("/invoice/delete-invoice", InvoiceDelete);
 module.exports = router;
